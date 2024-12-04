7fef461e3d68428ab33e1c977b5cc471a716a67e
NKC27 <93439579+NKC27@users.noreply.github.com>
2024-12-04 07:59:39 +0000

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/components/Navbar';
+import { Toaster } from '@/components/ui/toaster';
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+      <Toaster />
+    </main>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+    ],
+  },
+  experimental: {
+    ppr: 'incremental',
+    after: true,
+  },
+  appIsrStatus: true,
+  buildActivity: true,
+  buildActivityPosition: 'bottom-right',
 };
+
+/**
+ * @type {import('next').NextConfig}
+ */
+
+module.exports = nextConfig;
 
 export default nextConfig;
